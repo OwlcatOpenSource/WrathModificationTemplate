693925bb73ec58bd64ce14574fe2ec71d7c54f75
Sokolenko Alexander <sokolenko@owlcatgames.com>
2021-05-17 12:51:22 +0300

    fixed BuildAssemblies build step

--- Assets/Editor/Build/Tasks/BuildAssemblies.cs
+++ Assets/Editor/Build/Tasks/BuildAssemblies.cs
@@ -1,16 +1,21 @@
+using System;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 using OwlcatModification.Editor.Build.Context;
 using UnityEditor;
 using UnityEditor.Build.Pipeline;
 using UnityEditor.Build.Pipeline.Injector;
 using UnityEditor.Build.Pipeline.Interfaces;
 using UnityEditor.Build.Player;
+using UnityEditorInternal;
 
 namespace OwlcatModification.Editor.Build.Tasks
 {
 	public class BuildAssemblies : IBuildTask
 	{
+		private static readonly Regex AsmdefNameRegex = new Regex("\"name\":[\\s]*\"([^\"]+)\"");
+		
 		public int Version
 			=> 1;
 
@@ -33,7 +38,11 @@ namespace OwlcatModification.Editor.Build.Tasks
 			}
 
 			string[] asmdefGuids = AssetDatabase.FindAssets("t:Asmdef", new[] {m_ModificationParameters.ScriptsPath});
-			string[] asmdefNames = asmdefGuids.Select(AssetDatabase.GUIDToAssetPath).Select(Path.GetFileNameWithoutExtension).ToArray();
+			string[] asmdefNames = asmdefGuids
+				.Select(AssetDatabase.GUIDToAssetPath)
+				.Select(AssetDatabase.LoadAssetAtPath<AssemblyDefinitionAsset>)
+				.Select(GetAssemblyName)
+				.ToArray();
 			foreach (string filePath in Directory.GetFiles(outputFolder))
 			{
 				if (asmdefNames.All(i => !filePath.Contains(i)))
@@ -43,6 +52,17 @@ namespace OwlcatModification.Editor.Build.Tasks
 			}
 
 			return ReturnCode.Success;
+		}
+
+		private static string GetAssemblyName(AssemblyDefinitionAsset asmdef)
+		{
+			var m = AsmdefNameRegex.Match(asmdef.text);
+			if (!m.Success)
+			{
+				throw new Exception($"Assembly name is missing: {asmdef.name}");
+			}
+
+			return m.Groups[1].Value;
 		}
 	}
 }
